c7a3ddfe0d9fdf9c94f42201be0960267331a9d9
Cristian Porco <75568774+Cristian-Porco@users.noreply.github.com>
2022-12-03 17:44:06 +0100

    Setup Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<div class="full-site">
+    <nav id="navigation">
+        <div id="menuToggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Chi siamo</a></li>
+                <li class="buttonLogIn"><a href="#">Accedi</a></li>
+                <li class="buttonSignIn"><a href="#">Registrati</a></li>
+            </ul>
+        </div>
+        <div id="header">
+            <h1>
+                Secure Recipe
+            </h1>
+        </div>
+    </nav>
+    
+    <div id="content">
+        <form id="login">
+            <h2>Login</h2>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</div>
